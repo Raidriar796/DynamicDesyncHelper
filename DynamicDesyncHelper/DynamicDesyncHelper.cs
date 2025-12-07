0c4cc638589b250f03a68d3aa388e45f5042ae7c
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-12-06 16:10:03 -0800

    Project Setup

---
+++ DynamicDesyncHelper/DynamicDesyncHelper.cs
@@ -0,0 +1,22 @@
+﻿using ResoniteModLoader;
+using HarmonyLib;
+using FrooxEngine;
+
+namespace DynamicDesyncHelper;
+
+public partial class DynamicDesyncHelper : ResoniteMod
+{
+    public override string Name => "DynamicDesyncHelper";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/DynamicDesyncHelper";
+    private static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.DynamicDesyncHelper");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
